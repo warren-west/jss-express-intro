24d234c994cc766acc0f22caf4c3eb0a3b0242fb
warren-west <teacher.warren.english@gmail.com>
2025-01-13 11:34:50 +0100

    Added EJS and Middleware

---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+console.log("A log from app.js")
--- server.js
+++ server.js
@@ -5,19 +5,86 @@ const express = require('express')
 
 const app = express()
 
+// User logged in:
+const currentUser = {
+    username: "warren-west",
+    fName: "Warren",
+    lName: "West",
+    email: "warren.west@noroff.no",
+    age: 32,
+    isMarried: false
+}
+
+// User not logged in:
+// const currentUser = {}
+
+// Configure our app to use EJS (embedded JavaScript)
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views') // Where should Express find the views?
+
 // Overwrite the default behavior of the app
 // and grab any JSON body attached to a request
 app.use(express.json()) // it's a piece of middleware (more on middleware next week)
+app.use(express.static('public'))
 
+// #region Middleware
+// Using our own middleware functions:
+// Note the sequence is important!
+// app.use(myFirstMiddleware)
+// app.use(mySecondMiddleware)
+// app.use(printCountMiddleware)
+
+function myFirstMiddleware(req, res, next) {
+    // console.log("myFirstMiddleware() fired!")
+    req.count = 1
+    next()
+}
+
+function mySecondMiddleware(req, res, next) {
+    // console.log("mySecondMiddleware() fired!")
+    req.count += 1
+    next()
+}
+
+function printCountMiddleware(req, res, next) {
+    // console.log(`Value of count: ${req.count}`)
+    next()
+}
+
+function isUserLoggedIn(req, res, next) {
+    if (currentUser.username === undefined) {
+        res.redirect('/')
+    }
+
+    next()
+}
+
+// #endregion
+
+// #region Endpoints
 // GET /
 app.get('/', (req, res) => {
-    res.send("Hello world")
+    // res.send("Hello world")
+
+    // Serving static HTML:
+    // res.sendFile(__dirname + '/public/index.html')
+
+    // Serving dynamic EJS content (with a view engine):
+    res.render('index', {
+        fName: "Warren",
+        lName: "West",
+        message: "Welcome to EJS!"
+    })
+})
+
+app.get('/profile', isUserLoggedIn, (req, res) => {
+    res.render('userProfile', { currentUser })
 })
 
 // GET /students
 app.get('/students', (req, res) => {
     // /students?order=asc&lName=Lennox
-    console.log(req.query)
+    // console.log(req.query)
 
     const { lName, order } = req.query
     let studentList = [...students]
@@ -54,8 +121,8 @@ app.get('/students/:id', (req, res) => {
 
     const result = students.find((student) => student.id == req.params.id)
     if (!result)
-        res.status(404).json({error:`Error 404, the student with the id ${req.params.id} was not found.`})
-    
+        res.status(404).json({ error: `Error 404, the student with the id ${req.params.id} was not found.` })
+
     res.status(200).json(result)
 })
 
@@ -97,5 +164,7 @@ app.delete('/students/:id', (req, res) => {
 app.put('/students/:id', (req, res) => {
     console.log(`Updating student details with id: ${req.params.id}`)
 })
+
+// #endregion
 
 app.listen(3000)
